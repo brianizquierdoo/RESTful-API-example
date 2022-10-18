bbcc68674b55d7fd15a2e3d1c097be3c4a569120
jason <jasonkildare@jasons-mbp.attlocal.net>
2022-10-18 14:55:14 -0400

    new commit

--- springbootapp/src/main/java/com/example/springbootapp/Controller/springbootController.java
+++ springbootapp/src/main/java/com/example/springbootapp/Controller/springbootController.java
@@ -3,13 +3,16 @@ package com.example.springbootapp.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.springbootapp.Model.author;
+import com.example.springbootapp.Exception.ResourceNotFoundException;
+import com.example.springbootapp.Model.User;
 import com.example.springbootapp.Model.wishlist;
-import com.example.springbootapp.Respository.BooksRepository;
+import com.example.springbootapp.Respository.UserRepository;
 import com.example.springbootapp.Respository.WishlistRepository;
 
 @RestController
@@ -17,15 +20,24 @@ import com.example.springbootapp.Respository.WishlistRepository;
 public class springbootController {
 
 	@Autowired
-	private BooksRepository booksRepository;
+	private UserRepository UserRepository;
 	
-	@GetMapping("/author")
-	List<author> getAllBooks(){
-		return booksRepository.findAll();
+	@GetMapping("/user")
+	List<User> getAllUsers(){
+		return UserRepository.findAll();
+	}
+	
+	// Get User by Username
+	@GetMapping("/Users/{username}")
+	public ResponseEntity<User> getUserByUsername(@PathVariable(value = "username") String username) throws ResourceNotFoundException {
+		User user = UserRepository.findById(username)
+				.orElseThrow(() -> new ResourceNotFoundException("User not found for this username :: " + username));
+		return ResponseEntity.ok().body(user);
 	}
 	
 	@Autowired
 	private WishlistRepository wishlistRepository;
+	
 	@GetMapping("/wishlist")
 	List<wishlist> getAllWishlist(){
 		return wishlistRepository.findAll();
